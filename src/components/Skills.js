5ea46dd2888a582452e1a4ffa72f708e72a83949
priyampinju <pinjupriyam1826@gmail.com>
2024-01-24 22:37:11 +0530

    update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -37,7 +37,7 @@ const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p> My skill set encompasses a mastery of front end technologies, with a focus on creating seamless user experiences. Proficient in UI/UX design principles, I bring creativity and precision to every project</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
